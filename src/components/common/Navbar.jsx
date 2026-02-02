06744fd5f49c30b8a91d832f839a3c0c67c7e0bb
hanbe <hanbee4949@gmail.com>
2026-02-02 20:10:21 +0900

    style: navbar 수정

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -3,7 +3,6 @@ import { useAuth } from "../../contexts/AuthContext";
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../../assets/image.png";
 import { LogOut, User, LogIn, UserPlus } from "lucide-react";
 
 const menuItems = [
@@ -56,13 +55,62 @@ const Navbar = () => {
 
   return (
     <div className="navbar-wrapper">
-
-      <div className="navbar-top">
+      <div className="navbar-row">
+        {/* 로고 */}
         <div className="navbar-logo" onClick={() => navigate("/")}>
-          <img src={logo} alt="logo" className="logo-img" />
-          <span className="logo-text">WithChurch</span>
+          <span className="logo-text">withchurch</span>
         </div>
 
+        {/* 메뉴 */}
+        <div className="navbar-menu">
+          {menuItems.map((menu, index) => (
+            <div
+              className="menu-item"
+              key={menu.path}
+              onMouseEnter={() => {
+                clearTimeout(leaveTimer.current);
+                setActiveIndex(index);
+              }}
+              onMouseLeave={() => {
+                leaveTimer.current = setTimeout(() => setActiveIndex(null), 120);
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate(menu.path);
+              }}
+            >
+              <span>{menu.name}</span>
+
+              {menu.submenu.length > 0 && activeIndex === index && (
+                <div
+                  className="submenu"
+                  onMouseEnter={() => {
+                    clearTimeout(leaveTimer.current);
+                    setActiveIndex(index);
+                  }}
+                  onMouseLeave={() => {
+                    leaveTimer.current = setTimeout(() => setActiveIndex(null), 120);
+                  }}
+                >
+                  {menu.submenu.map((sub) => (
+                    <div
+                      key={sub.path}
+                      className="submenu-item"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        navigate(sub.path);
+                      }}
+                    >
+                      {sub.name}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+
+        {/*로그인, 회원가입*/}
         <div className="navbar-auth">
           {user ? (
             <>
@@ -73,92 +121,30 @@ const Navbar = () => {
                   navigate("/");
                 }}
               >
-                <LogOut size={26} />
+                <LogOut size={22} />
                 <span>로그아웃</span>
               </div>
-              <div
-                className="auth-item"
-                onClick={() => navigate("/profile")}
-              >
-                <User size={26} />
+
+              <div className="auth-item" onClick={() => navigate("/profile")}>
+                <User size={22} />
                 <span>프로필</span>
               </div>
             </>
           ) : (
             <>
-
-              <div
-                className="auth-item"
-                onClick={() => navigate("/login")}
-              >
-                <LogIn size={26} />
+              <div className="auth-item" onClick={() => navigate("/login")}>
+                <LogIn size={22} />
                 <span>로그인</span>
               </div>
 
-              <div
-                className="auth-item"
-                onClick={() => navigate("/signup/agree")}
-              >
-                <UserPlus size={26} />
+              <div className="auth-item" onClick={() => navigate("/signup/agree")}>
+                <UserPlus size={22} />
                 <span>회원가입</span>
               </div>
             </>
           )}
         </div>
       </div>
-
-      <div className="navbar-menu">
-        {menuItems.map((menu, index) => (
-          <div
-            className="menu-item"
-            key={index}
-            onMouseEnter={() => {
-              clearTimeout(leaveTimer.current);
-              setActiveIndex(index);
-            }}
-            onMouseLeave={() => {
-              leaveTimer.current = setTimeout(() => {
-                setActiveIndex(null);
-              }, 120);
-            }}
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(menu.path);
-            }}
-          >
-            <span>{menu.name}</span>
-
-            {menu.submenu.length > 0 && activeIndex === index && (
-              <div
-                className="submenu"
-                onMouseEnter={() => {
-                  clearTimeout(leaveTimer.current);
-                  setActiveIndex(index);
-                }}
-                onMouseLeave={() => {
-                  leaveTimer.current = setTimeout(() => {
-                    setActiveIndex(null);
-                  }, 120);
-                }}
-              >
-                {menu.submenu.map((sub, i) => (
-                  <div
-                    key={i}
-                    className="submenu-item"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      navigate(sub.path);
-                    }}
-                  >
-                    {sub.name}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-
     </div>
   );
 };
